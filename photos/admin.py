1a6fc367a4c91ec68e7531e48ba53cd1bfdd18bb
YD KIM <youngdong9800@gmail.com>
2019-12-09 12:52:45 +0900

    requirements edit

--- photos/admin.py
+++ photos/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from django.utils.html import mark_safe
-from . import models
 
-@admin.register(models.Photo)
-class CustomPhoto(admin.ModelAdmin):
-
-    """ Custom Photo Admin """
-
-    list_display = ("__str__", "get_thumbnail")
-
-    def get_thumbnail(self, obj):
-        print(obj.photo)
-        return mark_safe(f'<img src="{obj.photo.url}" />')
-
-    get_thumbnail.short_description = "Thumbnail"
-
-@admin.register(models.Photo_info)
-class CustomPhotoInfo(admin.ModelAdmin):
-
-    pass
+# Register your models here.
