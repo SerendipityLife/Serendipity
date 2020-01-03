f7dba2e85372af1a1b319bd90482541fb6cd086f
Daehyeop Ko <daehyeop.ko@gmail.com>
2020-01-03 18:39:53 +0000

    added dev dummpy status app for remote mysql db connection test

--- status/views.py
+++ status/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from status.models import TestDb
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the status index.")
+    return HttpResponse('Hello, world. You\'re at the status index.')
+
+def insertTestDb(request):
+    name = request.GET.get('name')
+    TestDb(name=name).save()
+    return render(request, 'status/test.html', {'testDb': 'inserted Status Data -> ' + name})
