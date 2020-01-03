f06b034ecc4782c6335c8f589ab8a3cb4eaa595f
Daehyeop Ko <daehyeop.ko@gmail.com>
2020-01-03 18:35:28 +0000

    changed default database: sqlite3 -> mysql5.7 and added reverse proxy sever

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -45,6 +44,9 @@ PROJECT_APPS = [
     "histories.apps.HistoriesConfig",
     "photos.apps.PhotosConfig",
     "wordfilters.apps.WordfiltersConfig",
+    "status.apps.StatusConfig",
+    "fanmanage.apps.FanmanageConfig",
+    "influencermanage.apps.InfluencermanageConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
@@ -85,8 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'test01',
+        'USER': 'root',
+        'PASSWORD': 'rootpass',
+        'HOST': 'database1',
+        'PORT': '3306'
     }
 }
 
@@ -127,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "users.User"
